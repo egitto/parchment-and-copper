9fdd1844ff469dad56ec7c2990a43c6cd8ac7ca5
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-05-24 14:04:38 -0400

    added readme, improved CLI

--- talmud_estate_division/talmud_estate_division.py
+++ talmud_estate_division/talmud_estate_division.py
@@ -33,8 +33,24 @@ def divide_estate_test():
 		expected = i[1]
 		assert actual == expected, print("expected",expected,"\nactual",actual) 
 
-estate = Fract(input("Enter estate to split: "))
-c_string = input("Enter sorted list of creditors: ").replace(","," ").replace("  "," ")
-creditors = [Fract(i) for i in c_string.split()]
-paid = [float(i) for i in divide_estate(estate,creditors)]
-print("Distribution of funds: ",*paid)
+if __name__ == '__main__':
+	import sys, re
+	def print_help(): 
+		s = ['no arguments: prints help string',
+		'\n-f: total estate funds to allocate',
+		'\n-c: sorted comma-separated list of debts',
+		'\nusage example:',
+		'\n   talmud_estate_division.py -f 200 -c 100,200,300']
+		print(*s)
+	creditors = estate = None
+	prev_arg = ''
+	for i in sys.argv[1:]:
+		if prev_arg == '-f':
+			if re.fullmatch('\d+(\.\d*)?',i):
+				estate = Fract(i)
+		elif prev_arg == '-c':
+			if re.fullmatch('(\d+(\.\d*)?,)*\d+(\.\d*)?',i):
+				creditors = [Fract(j) for j in i.split(',')]
+		prev_arg = i
+	if creditors and estate: print(str([float(i) for i in divide_estate(estate,creditors)])[1:-1])
+	else: print_help()
