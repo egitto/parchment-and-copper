48e68d018a822c9e66dab104abdac8fc946c3a90
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-07-15 00:29:26 -0400

    added readme. refactored some

--- thorium/thorium.js
+++ thorium/thorium.js
@@ -5,12 +5,11 @@ fs = require('fs')
 var variable_storage_path = 'thorium_variables.json'
 var auth_discord_thorium = JSON.parse(fs.readFileSync('auth.thorium.json'))['auth_discord_thorium']
 client.login(auth_discord_thorium)
-var default_parameters = {"watched_emojii":["🗒","🤖","📓","📔"],"log_channel_name":"topicis-logs","threshold":2,"obey_roles":["consuls","conifer","mods","moderators"],"flag_logged_emoji":"🗒"}
-var unchangeable_owners = ["consuls","conifer"]
+var default_parameters = {"watched_emojii":["🗒","🤖","📓","📔"],"log_channel_name":"topicis-logs","threshold":2,"obey_roles":["mods","moderators"],"flag_logged_emoji":"🗒","obey_forever_roles":["consuls","conifer"]}
 
-Set.prototype.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
+// Set.prototype.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
 
-x = ''+fs.readFileSync(variable_storage_path)
+x = '' + fs.readFileSync(variable_storage_path)
 if (!x){
 	x = default_parameters
 	console.log('using defaults...')
@@ -24,23 +23,34 @@ client.on('ready', () => {
 
 var watched_emojii = new Set(x.watched_emojii)
 var obey_roles = new Set(x.obey_roles)
+obey_roles.toJSON = watched_emojii.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
 var threshold = x.threshold
 var log_channel_name = x.log_channel_name
 var flag_logged_emoji = x.flag_logged_emoji
-var all_data = {watched_emojii: watched_emojii, log_channel_name: log_channel_name, threshold: threshold, obey_roles: obey_roles, flag_logged_emoji: flag_logged_emoji}
+var obey_forever_roles = x.obey_forever_roles
+var all_data = {watched_emojii: watched_emojii, log_channel_name: log_channel_name, threshold: threshold, obey_roles: obey_roles, flag_logged_emoji: flag_logged_emoji, obey_forever_roles: obey_forever_roles}
 
 function set_log_channel_name(name){
 	all_data.log_channel_name = log_channel_name = name
-	console.log('log channel changed to '+name)	
+	console.log('log channel changed to ' + name)	
 }
 
-function change_threshold(value){
+function set_threshold(value){
 	threshold = all_data.threshold = value
-	console.log('threshold changed to '+value)
+	console.log('threshold changed to ' + value)
+}
+
+function array_to_string(arr){
+	return arr.reduce(function(accumulator,item){return accumulator + ', ' + item})
+}
+
+function set_to_string(set){
+	return [...set].reduce(function(accumulator,item){return accumulator + ', ' + item})
 }
 
 function save_parameters(){
-	fs.writeFile(variable_storage_path,JSON.stringify(all_data))
+	var JSON_pretty = x => JSON.stringify(x,null,'  ')
+	fs.writeFile(variable_storage_path,JSON_pretty(all_data))
 }
 
 console.log(JSON.stringify(all_data))
@@ -48,14 +58,14 @@ console.log('loading...')
 
 client.on('message', msg => {
 	if (msg.content.match(/^[Tt]horium$/)) {
-    safe_reply(msg, 'I can serve. \nthreshold: '+threshold+'\nwatched emoji:  '+[...watched_emojii]+'\ncontrol roles: '+[...obey_roles]+'\nlog channel: '+log_channel_name)
+    reply(msg, 'I can serve. \nthreshold: ' + threshold + '\nwatched emoji:  ' + set_to_string(watched_emojii) + '\ncontrol roles: ' + set_to_string(obey_roles) + '\nlog channel: ' + log_channel_name)
     // console.log(msg.channel.guild)
   }
   // console.log(msg.member.roles)
   var obey_this = !!(msg.member.roles.find(function(item){return obey_roles.has(item.name)}, true))
-  console.log(obey_this+' '+msg.member.user.username+'/'+msg.member.nickname+'\n  '+msg.content)
+  console.log(obey_this + ' ' + msg.member.user.username + '/' + msg.member.nickname + '\n  ' + msg.content)
 	if (obey_this &&(msg.content.match(/^[Tt]horium [^{}()\\]*$/))){
-		safe_reply(msg,parse_command_phrase(msg.content.replace(/^[Tt]horium /,'')))
+		reply(msg,parse_command_phrase(msg.content.replace(/^[Tt]horium /,'')))
 		save_parameters()
 	}
 }) 
@@ -64,41 +74,41 @@ function parse_command_phrase(phrase){
 		// what follows is disgusting, avert your eyes.
 	  var x = /^threshold (\d+)$/
 		if (phrase.match(x)) {
-			change_threshold(phrase.replace(x,'$1'))
-			return ('New threshold: '+threshold)
+			set_threshold(phrase.replace(x,'$1'))
+			return 'New threshold: ' + threshold
 		}
 		var x = /^watch (.+)$/
 		if (phrase.match(x)) {
 			watched_emojii.add(phrase.replace(x,'$1'))
-			return ('New trigger emoji: '+[...watched_emojii])
+			return 'New trigger emoji: ' + set_to_string(watched_emojii)
 		}
 		var x = /^unwatch (.+)$/
 		if (phrase.match(x)) {
 			watched_emojii.delete(phrase.replace(x,'$1'))
-			return ('New trigger emoji: '+[...watched_emojii])
+			return 'New trigger emoji: ' + set_to_string(watched_emojii)
 	  }
 	  var x = /^obey (.+)$/
 		if (phrase.match(x)) {
 			obey_roles.add(phrase.replace(x,'$1'))
-			return ('New control roles: '+[...obey_roles])
+			return 'New control roles: ' + set_to_string(obey_roles)
 	  }
 	  var x = /^disobey (.+)$/
 		if (phrase.match(x)) {
 			obey_roles.delete(phrase.replace(x,'$1'))
-			unchangeable_owners.map(function(owner){obey_roles.add(owner)})
-			return ('New control roles: '+[...obey_roles])
+			obey_forever_roles.map(function(owner){obey_roles.add(owner)})
+			return 'New control roles: ' + set_to_string(obey_roles)
 		}
 	  var x = /^change log channel (.+)$/
 		if (phrase.match(x)) {
 			set_log_channel_name(phrase.replace(x,'$1'))
-			return ('New channel: '+log_channel_name)
+			return 'New channel: ' + log_channel_name
 		}
 		if (phrase.match(/^help/)) {
-			return ('I can be controlled with these words: \nthreshold $number\nwatch $emoji\nunwatch $emoji\nobey $role\ndisobey $role\nchange log channel $channel\nhelp')
+			return 'I can be controlled with these words: \nthreshold $number\nwatch $emoji\nunwatch $emoji\nobey $role\ndisobey $role\nchange log channel $channel\nhelp'
 		}
 }
 
-function safe_reply(msg,content){
+function reply(msg,content){
 	msg.reply(content,{disableEveryone: true}) 
 }
 
@@ -127,19 +137,19 @@ function isnt_logged_yet(msg){
 client.on('messageReactionAdd', messageReaction => {
 	var emoji_name = messageReaction._emoji.name 
 	var reacts = messageReaction.count
-	console.log('reaction spotted: '+emoji_name+' in #'+messageReaction.message.channel.name)
+	console.log('reaction spotted: ' + emoji_name + ' in #' + messageReaction.message.channel.name)
 	msg = messageReaction.message
-	if(is_watched(emoji_name) && (reacts >= threshold) && isnt_logged_yet(msg) && (''+msg)){
+	if(is_watched(emoji_name) && (reacts >= threshold) && isnt_logged_yet(msg) && ('' + msg)){
 		// msg = messageReaction.message
 		console.log(emoji_name)
-		var reply = emoji_name+' at '+msg.createdAt+' in '+msg.channel +', ' +msg.author+' said:\n'+msg
+		var reply = emoji_name + ' at ' + msg.createdAt + ' in ' + msg.channel +', ' +msg.author + ' said:\n' + msg
 		// console.log(msg)
 		if (log_channel(msg)){
 			msg.react(flag_logged_emoji)
 			log_channel(msg).send(reply,{disableEveryone: true}) 
-			console.log('recorded '+reply)
+			console.log('recorded ' + reply)
 		}else{
-			report_error_on_servermeta(msg,'log channel '+log_channel_name+' not found')
+			report_error_on_servermeta(msg,'log channel ' + log_channel_name + ' not found')
 		}
 	}
 }) 
