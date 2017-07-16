b627c8017acd79ff6f6b84454d661c9e50cd3980
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-07-16 00:53:53 -0400

    refactored, put all global variables in single global object

--- thorium/thorium.js
+++ thorium/thorium.js
@@ -9,42 +9,37 @@ client.login(auth_discord_thorium)
 
 // Set.prototype.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
 
-x = '' + fs.readFileSync(variable_storage_path)
-x = JSON.parse(x)
 
-var watched_emojii = new Set(x.watched_emojii)
-var obey_roles = new Set(x.obey_roles)
-obey_roles.toJSON = watched_emojii.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
-var threshold = x.threshold
-var log_channel_name = x.log_channel_name
-var flag_logged_emoji = x.flag_logged_emoji
-var obey_forever_roles = x.obey_forever_roles
-var all_data = {watched_emojii: watched_emojii, log_channel_name: log_channel_name, threshold: threshold, obey_roles: obey_roles, flag_logged_emoji: flag_logged_emoji, obey_forever_roles: obey_forever_roles}
-
-function set_log_channel_name(name){
-  all_data.log_channel_name = log_channel_name = name
-  console.log('log channel changed to ' + name)  
+function set_globals_from_json(json){
+  set_globals_from_json(json)
 }
 
-function set_threshold(value){
-  threshold = all_data.threshold = value
-  console.log('threshold changed to ' + value)
+function set_globals_from_json(json){
+  var x = JSON.parse(json)
+  globals = x
+  // globals.log_channel_name = x.log_channel_name
+  // globals.threshold = x.threshold
+  // globals.obey_forever_roles = x.obey_forever_roles
+  globals.watched_emojii = new Set(x.watched_emojii)
+  globals.obey_roles = new Set(x.obey_roles)
+  globals.obey_roles.toJSON = globals.watched_emojii.toJSON = function toJSON() {return [...Set.prototype.values.call(this)]}
+  console.log(json)
 }
+
+set_globals_from_json('' + fs.readFileSync(variable_storage_path))
 
 function array_to_string(arr){
   return arr.reduce(function(accumulator,item){return accumulator + ', ' + item})
 }
 
-function set_to_string(set){
+function set_to_pretty_string(set){
   return [...set].reduce(function(accumulator,item){return accumulator + ', ' + item})
 }
 
 var JSON_pretty = x => JSON.stringify(x,null,'  ')
-function save_parameters(){
-  fs.writeFileSync(variable_storage_path,JSON_pretty(all_data))
-}
+var save_parameters = () => fs.writeFileSync(variable_storage_path,JSON_pretty(globals))
 
-console.log(JSON.stringify(all_data))
+console.log(JSON.stringify(globals))
 console.log('loading...')
 
 client.on('ready', () => {
@@ -53,10 +48,10 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content.match(/^[Tt]horium$/)) {
-    reply(msg, 'I can serve. \nthreshold: ' + threshold + '\nwatched emoji:  ' + set_to_string(watched_emojii) + '\ncontrol roles: ' + set_to_string(obey_roles) + '\nlog channel: ' + log_channel_name)
+    reply(msg, 'I can serve. \nthreshold: ' + globals.threshold + '\nwatched emoji:  ' + set_to_pretty_string(globals.watched_emojii) + '\ncontrol roles: ' + set_to_pretty_string(globals.obey_roles) + '\nlog channel: ' + globals.log_channel_name)
     // console.log(msg.channel.guild)
   }
-  x = /^topicis: (.*)/
+  x = /^topicis:? (.*)/
   if (msg.content.match(x)){
     // we don't want the bot to say 'topicis' because that interferes with users searching for that string
     var new_message = Object.create(msg)
@@ -64,7 +59,7 @@ client.on('message', msg => {
     log_message(new_message)
   }
   // console.log(msg.member.roles)
-  var obey_this = !!(msg.member.roles.find(function(item){return obey_roles.has(item.name)}, true))
+  var obey_this = !!(msg.member.roles.find(item => {return globals.obey_roles.has(item.name)}, true))
   console.log(obey_this + ' ' + msg.member.user.username + '/' + msg.member.nickname + '\n  ' + msg.content)
   if (obey_this &&(msg.content.match(/^[Tt]horium [^{}()\\]*$/))){
     reply(msg,parse_command_phrase(msg.content.replace(/^[Tt]horium /,'')))
@@ -74,25 +69,26 @@ client.on('message', msg => {
 
 function parse_command_phrase_curried(phrase){
     // lmao this is even worse than before
-    var parse = curry(function(new_parameter_regex,text_response_function,new_value_function,keyword_regex,parameter_name){
+    var parse = curry((new_parameter_regex,text_response_function,new_value_function,keyword_regex,parameter_name)=>{
       if (!phrase.match(keyword_regex)) {return}
-      if (! (x = phrase.match(new_parameter_regex))) {return}
+      var x = phrase.match(new_parameter_regex)
+      if (!x) {return}
       x = x[0]
-      all_data[parameter_name] = y = new_value_function(x,all_data[parameter_name])
+      globals[parameter_name] = y = new_value_function(x,globals[parameter_name])
       return {val: y, text: text_response_function(y)}
     })
-    threshold = parse(/[\d]+$/)(x => {x})(x => {x})(/^threshold .*/)('threshold') || threshold
-    log_channel_name = parse(/[^ ]*$/)(x => {x})(x => {x})(/^change log channel .*/)('log_channel_name') || log_channel_name
+    globals.threshold = parse(/[\d]+$/)(x => {x})(x => {x})(/^threshold .*/)('threshold') || globals.threshold
+    globals.log_channel_name = parse(/[^ ]*$/)(x => {x})(x => {x})(/^change log channel .*/)('log_channel_name') || globals.log_channel_name
     var new_parameter_regex_base = parse(/[^\b]*$/)
-    var set_base = new_parameter_regex_base((x,new_value)=>set_to_string(new_value))
+    var set_base = new_parameter_regex_base((x,new_value)=>set_to_pretty_string(new_value))
     var add_set = set_base((x,old_value)=>old_value.add(x))
 
-    watched_emojii = add_set(/^watch .*/)('watched_emojii') || watched_emojii
-    obey_roles = add_set(/^obey .*/)('obey_roles') || obey_roles
+    globals.watched_emojii = add_set(/^watch .*/)('watched_emojii') || globals.watched_emojii
+    globals.obey_roles = add_set(/^obey .*/)('obey_roles') || globals.obey_roles
 
     var subtract_set = set_base((x,old_value)=>old_value.delete(x))
-    watched_emojii = subtract_set(/^watch .*/)('watched_emojii') || watched_emojii
-    obey_roles = subtract_set(/^obey .*/)('obey_roles') || obey_roles
+    globals.watched_emojii = subtract_set(/^watch .*/)('watched_emojii') || globals.watched_emojii
+    globals.obey_roles = subtract_set(/^obey .*/)('obey_roles') || globals.obey_roles
 
 
 }
@@ -101,37 +97,37 @@ function parse_command_phrase(phrase){
     // what follows is disgusting, avert your eyes.
     var x = /^threshold (\d+)$/
     if (phrase.match(x)) {
-      set_threshold(phrase.replace(x,'$1'))
-      return 'New threshold: ' + threshold
+      globals.threshold = phrase.replace(x,'$1')
+      return 'New threshold: ' + globals.threshold
     }
     var x = /^watch (.+)$/
     if (phrase.match(x)) {
-      watched_emojii.add(phrase.replace(x,'$1'))
-      return 'New trigger emoji: ' + set_to_string(watched_emojii)
+      globals.watched_emojii.add(phrase.replace(x,'$1'))
+      return 'New watched emoji: ' + set_to_pretty_string(globals.watched_emojii)
     }
     var x = /^unwatch (.+)$/
     if (phrase.match(x)) {
-      watched_emojii.delete(phrase.replace(x,'$1'))
-      return 'New trigger emoji: ' + set_to_string(watched_emojii)
+      globals.watched_emojii.delete(phrase.replace(x,'$1'))
+      return 'New watched emoji: ' + set_to_pretty_string(globals.watched_emojii)
     }
     var x = /^obey (.+)$/
     if (phrase.match(x)) {
-      obey_roles.add(phrase.replace(x,'$1'))
-      return 'New control roles: ' + set_to_string(obey_roles)
+      globals.obey_roles.add(phrase.replace(x,'$1'))
+      return 'New control roles: ' + set_to_pretty_string(globals.obey_roles)
     }
     var x = /^disobey (.+)$/
     if (phrase.match(x)) {
-      obey_roles.delete(phrase.replace(x,'$1'))
-      obey_forever_roles.map(function(owner){obey_roles.add(owner)})
-      return 'New control roles: ' + set_to_string(obey_roles)
+      globals.obey_roles.delete(phrase.replace(x,'$1'))
+      globals.obey_forever_roles.map(owner => {globals.obey_roles.add(owner)})
+      return 'New control roles: ' + set_to_pretty_string(globals.obey_roles)
     }
     var x = /^change log channel (.+)$/
     if (phrase.match(x)) {
-      set_log_channel_name(phrase.replace(x,'$1'))
-      return 'New channel: ' + log_channel_name
+      globals.log_channel_name = phrase.replace(x,'$1')
+      return 'New channel: ' + globals.log_channel_name
     }
     if (phrase === 'dump parameters') {
-      return '```'+JSON_pretty(all_data)+'```'
+      return '```'+JSON_pretty(globals)+'```'
     }
     if (phrase === 'shut down without confirmation') {
       throw 'shut down by command'
@@ -146,7 +142,7 @@ function reply(msg,content){
 }
 
 function log_channel(msg){
-  return msg.guild.channels.find('name', log_channel_name)
+  return msg.guild.channels.find('name', globals.log_channel_name)
 }
 
 function report_error_on_servermeta(msg,errormessage){
@@ -157,7 +153,7 @@ function report_error_on_servermeta(msg,errormessage){
 }
 
 function is_watched(emoji){
-  return watched_emojii.has(emoji)
+  return globals.watched_emojii.has(emoji)
   // return true
 }
 
@@ -170,15 +166,15 @@ function isnt_logged_yet(msg){
 function log_message(msg,emoji_name){
     // msg = messageReaction.message
   console.log(emoji_name)
-  emoji_name = emoji_name || flag_logged_emoji
+  emoji_name = emoji_name || globals.flag_logged_emoji
   var reply = emoji_name + ' at `' + msg.createdAt + '` in ' + msg.channel +', ' +msg.author + ' said:\n\n' + msg
   // console.log(msg)
   if (log_channel(msg)){
-    msg.react(flag_logged_emoji)
+    msg.react(globals.flag_logged_emoji)
     log_channel(msg).send(reply,{disableEveryone: true}) 
     console.log('recorded ' + reply)
   }else{
-    report_error_on_servermeta(msg,'log channel ' + log_channel_name + ' not found')
+    report_error_on_servermeta(msg,'log channel ' + globals.log_channel_name + ' not found')
   }
 }
 
@@ -187,7 +183,7 @@ client.on('messageReactionAdd', messageReaction => {
   var reacts = messageReaction.count
   console.log('reaction spotted: ' + emoji_name + ' in #' + messageReaction.message.channel.name)
   msg = messageReaction.message
-  if(is_watched(emoji_name) && (reacts >= threshold) && isnt_logged_yet(msg) && ('' + msg)){
+  if(is_watched(emoji_name) && (reacts >= globals.threshold) && isnt_logged_yet(msg) && ('' + msg)){
     log_message(msg,emoji_name)
   }
 }) 
