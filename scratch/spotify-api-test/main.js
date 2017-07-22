9d3073077b30f3ea74a44e92040fcc6b0e23b0f5
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-07-22 10:42:48 -0400

    added request, added depagination and actual functionality

--- scratch/spotify-api-test/main.js
+++ scratch/spotify-api-test/main.js
@@ -1,31 +1,54 @@
-spotify = new (require('spotify-web-api-node'))()
-fs = require('fs')
-opn = require('opn')
-
-const read = x=> fs.readFileSync((x[0].replace(/^~(?=\/|$)/,process.env.HOME)))    // read`~/file`
-const write = (x,y)=> fs.writeFileSync(x.replace(/^~(?=\/|$)/,process.env.HOME),y) // write('~/file',stuff)
-const JSON_pretty = x => JSON.stringify(x,null,'  ')
-
+var spotify = new (require('spotify-web-api-node'))()
+var fs = require('fs')
+var opn = require('opn')
+var request = require('request')
+var pathify = str=> str.replace(/^~(?=\/|$)/,process.env.HOME)
+var read = x=> fs.readFileSync(pathify(x[0]))    // read`~/file`
+var write = (x,y)=> fs.writeFileSync(pathify(x),y) // write('~/file',stuff)
+var JSON_pretty = x => JSON.stringify(x,null,'  ')
 var set_creds = () => spotify.setCredentials(JSON.parse(read`~/.auth/spotiman`))
-
 var update_creds = new_creds =>{
-	old = spotify.getCredentials()
+	console.log('updating creds')
+	var old = spotify.getCredentials()
 	new_creds.access_token && (old.accessToken = new_creds.access_token)
 	new_creds.refresh_token && (old.refreshToken = new_creds.refresh_token)
 	spotify.setCredentials(old)
 	write('~/.auth/spotiman',JSON_pretty(spotify.getCredentials()))
 }
-
-a = process.argv
-if (a[2] == 'auth'){
-	opn( spotify.createAuthorizeURL(['playlist-read-private','playlist-read-collaborative','playlist-modify-public','playlist-modify-private','user-library-read','user-library-modify']) )
-}else if (a[2] == 'auth2') {
+function get_url(url){request.get({url: url, headers: { 'Authorization': 'Bearer ' + spotify.getCredentials().accessToken }, json: true},x=>{return promise.resolve(x)})}
+var depaginate = (href) => {
+	console.log(href)
+	return get_url(href).then(page=>{
+		page = page.body
+		if (!page.next) {return promise.resolve(page.items)}
+		return depaginate(page.next,fetch).then(x=>promise.resolve(page.items.concat(x)))})
+}
+var do_spotify_stuff = () => {
+	console.log('doing spotify stuff')
+	spotify.getUserPlaylists().then(x=>x.body.items.slice(0,1).map(pl=>{
+		var uri_regex = /^spotify:user:(\d+):\w+:([^:]+)$/
+		var user = pl.uri.replace(uri_regex,'$1')
+		var pl = pl.id
+		spotify.getPlaylistTracks(user,pl, {limit: 5}).then(x=>{
+			depaginate(x.body.href).then(x=>console.log(x))
+			console.log(x.body)
+		})
+	}))
+}
+var a = process.argv
+set_creds()
+if (a[2] === 'auth'){
+	opn(spotify.createAuthorizeURL(['playlist-read-private','playlist-read-collaborative','playlist-modify-public','playlist-modify-private','user-library-read','user-library-modify']))
+}else if (a[2] === 'auth2') {
 	spotify.authorizationCodeGrant(a[3]).then(x=>{
+		console.log('authcodeblock')
 		x = x.body
 		update_creds(x)
-	}).done()
+	})
 }else if (!a[2]){
-	spotify.refreshAccessToken().then(x=>{x = x.body; update_creds(x)
+	spotify.refreshAccessToken().then(x=>{
+		x = x.body
+		update_creds(x)
 		do_spotify_stuff()
-	}).done()
+	})
 }
