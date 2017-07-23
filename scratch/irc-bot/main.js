9c8a9a198bbc8b77865bd3b3400f0722d0efae21
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-07-23 15:35:45 -0400

    started irc bot

---
+++ scratch/irc-bot/main.js
@@ -0,0 +1,66 @@
+irc = require('irc')
+fs = require('fs')
+discord = require('discord.js')
+
+console.log('creating irc client')
+irc_auth = JSON.parse(fs.fileReadSync('irc_auth'))
+iclient = new irc.Client(irc_auth.server,irc_auth.nickname,{channels: irc_auth.channels})
+
+console.log('creating discord client')
+discord_auth = JSON.parse(fs.readFileSync('discord_auth'))['token']
+dclient = new discord.Client()
+dclient.login(discord_auth).catch((err)=>{console.log('discord login error'); throw err})
+
+console.log('reading connection config')
+connections_path = 'irc_discord_connections'
+connections = JSON.parse(fs.fileReadSync(connections_path))
+// just to demo what it should look like; delete this line later
+connections = [{irc_server: 'something.net', irc_channel: '#rational', discord_guild: '234827', discord_channel: '273872'}]
+var JSON_pretty = x => JSON.stringify(x,null,'  ')
+var write_connections = () => fs.fileWriteSync(connections_path, JSON_pretty(connections))
+var add_connection = (json_string) => {
+	if json_string.match(/^\{\s*"irc_server":\s*"[\w]+\.[\w]+\",\s*"irc_channel":\s*"#[\w0-9]+",\s*"discord_guild":\s*"\d+",\s*"discord_channel":\s*"\d+"\s*\}$/){
+		connections.push(JSON.parse(json_string))
+		write_connections()
+	}
+}
+
+console.log('setting irc listeners')
+irc_auth.channels.map(channel=>{
+	iclient.addListener('message'+channel, function(from,to,message){
+		console.log(channel+': '+from+' => '+to+': '+message)
+		on_irc_message(channel,from,to,message)
+})}
+iclient.addListener('pm', function(from,message){
+	console.log(from+' => irc-bot 'message)
+	on_irc_pm(from,message)
+})
+
+console.log('setting discord listeners')
+dclient.on('ready', () => {
+  console.log('discord logged in')
+})
+dclient.on('message', message => {
+
+}
+
+
+function on_irc_message(channel,from,to,message){
+	var on_irc_message_channel
+	switch(channel){
+		case channel === '#lw': on_irc_message_channel = on_irc_message_lw
+		case channel === '#rational': on_irc_message_channel = on_irc_message_rational
+	}
+	on_irc_message_channel && on_irc_message_channel(from,to,message)
+}
+function on_irc_message_lw(from,to,message){
+}
+function on_irc_message_rational(from,to,message){
+}
+
+function send_irc_to_discord(channel,from,to,message){
+
+}
+function send_discord_to_irc(message){
+	
+}
