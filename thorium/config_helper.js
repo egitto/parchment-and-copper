c133ab222a997cda81d18d3ef175213549ad558b
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-09 11:51:05 -0400

    converted objects to strings

--- thorium/config_helper.js
+++ thorium/config_helper.js
@@ -8,7 +8,7 @@ function input1(){
   console.log("To configure Thorium, you will need to register a bot at https://discordapp.com/developers/applications/me")
   console.log("Once there, select 'create a new app'\n Give it a name and a description, then save changes and click 'create a new bot user.'\n Copy down your Client ID and your Bot's token.\n\nopen https://discordapp.com/developers/applications/me now? Y/n")
   stdin.once("data",(d)=>{
-    var d = d.slice(0,d.length-1)
+    var d = ''+d.slice(0,d.length-1)
     if(d == "" || d == "Y" || d == "y" || d == "yes"){
       opn("https://discordapp.com/developers/applications/me")
       input2()
@@ -24,7 +24,7 @@ function input1(){
 function input2(){
   console.log("Enter your client id:")
   stdin.once("data",(d)=>{
-    var d = d.slice(0,d.length-1)
+    var d = ''+d.slice(0,d.length-1)
     auth.client_id = d
     auth.register_on_guild = "https://discordapp.com/oauth2/authorize?&client_id="+d+"&scope=bot&permissions=0"
     input3()
@@ -34,7 +34,7 @@ function input2(){
 function input3(){
   console.log("Enter your bot's token:")
   stdin.once("data",(d)=>{
-    var d = d.slice(0,d.length-1)
+    var d = ''+d.slice(0,d.length-1)
     auth.auth_discord_thorium = d
     input4()
   })
@@ -43,7 +43,7 @@ function input3(){
 function input4(){
   console.log("Create new authentication file using token and client id? Y/n")
   stdin.once("data",(d)=>{
-    var d = d.slice(0,d.length-1)
+    var d = ''+d.slice(0,d.length-1)
     if(d == "" || d == "Y" || d == "y" || d == "yes"){
       fs.writeFileSync(authpath, JSON.stringify(auth))
       input5()
@@ -59,7 +59,7 @@ function input4(){
 function input5(){
   console.log("Add bot to a guild/server? Requires you to have input client id. Y/n")
   stdin.once("data",(d)=>{
-    var d = d.slice(0,d.length-1)
+    var d = ''+d.slice(0,d.length-1)
     if(d == "" || d == "Y" || d == "y" || d == "yes"){
       opn(auth.register_on_guild)
       input6()
