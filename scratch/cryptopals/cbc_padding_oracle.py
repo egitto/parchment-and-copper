07fee39746fc3547d4f3b2155ecebe174dead496
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-11 19:24:58 -0400

    realized I was forgetting to xor my attack with the _actual_ block

--- scratch/cryptopals/cbc_padding_oracle.py
+++ scratch/cryptopals/cbc_padding_oracle.py
@@ -3,15 +3,15 @@ from cbc import CBC_encrypt, CBC_decrypt
 from padPKCS7 import pad, unpad, PaddingError
 import random
 
+key = data(b"YELLOW SUBMARINE").bytes
 # key = int.to_bytes(random.getrandbits(16*8),16,'big') # I really need to put this in a library somewhere
-key = data(b"asdfasdfasdfasdf").bytes
 def encrypt_one_token():
-  tokens = ['MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
+  tokens = ['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA','MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
   'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=','MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==','MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==','MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl','MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==','MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==','MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=','MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=','MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93']
-  # token = data(random.sample(tokens,1)[0]).bytes
   token = data(tokens[0]).bytes
+  # token = data(random.sample(tokens,1)[0]).bytes
+  iv = key[::-1]
   # iv = int.to_bytes(random.getrandbits(16*8),16,'big')
-  iv = key
   return (iv,CBC_encrypt(pad(token,16),key,iv))
 
 def check_padding(iv,token):
@@ -22,21 +22,33 @@ def check_padding(iv,token):
 
 # print(check_padding(*encrypt_one_token()))
 
-def break_block(block,oracle):
+def break_block(block,previous_block,oracle):
+  """Decrypts a block encrypted with CBC using a padding oracle. written to use check_padding.
+  """
   right_bytes = b'' # invariant: rightbytes is the rightmost known bytes of plaintext.
-  def l(): return len(right_bytes)
-  # to check if the last byte is '\x01': 
-  def leftpad(_bytes):
-    return b'\x00'*(16-len(_bytes))+_bytes
+  l = 0
+  def leftpad(_bytes): return b'\x00'*(16-len(_bytes))+_bytes
   all_bytes = [int.to_bytes(x,1,'big') for x in range(256)]
-  while l() < 16:
-    padmask1 = pad(b'\x00'*(15-l())) # both padmasks have correct padding
-    padmask2 = pad(b'\xFF'*(15-l())) # therefore, (padmask ^ plaintext ^ (leftpad(right_bytes))) has correct padding
+  while l < 16:
+    padmask1 = pad(b'\x00'*(15-l)) # both padmasks have correct padding
+    padmask2 = pad(b'\xFF'*(15-l)) # therefore, (padmask ^ plaintext ^ (leftpad(right_bytes))) has correct padding
     for x in all_bytes:
-      test_bytes = leftpad(x+right_bytes)
+      test_bytes = xor(leftpad(x+right_bytes),previous_block) #DUH
       if oracle(xor(test_bytes,padmask1),block) and oracle(xor(test_bytes,padmask2),block):
         right_bytes = x+right_bytes
+        print(l)
         break
-  return xor(right_bytes,(b'\xFF'*16))
+    l += 1
+  return xor(right_bytes,(b'\x00'*16))
+
+def steal_last_byte(block,oracle):
+  all_bytes = [int.to_bytes(x,1,'big') for x in range(256)]
+  def lp(_bytes): return b'\x00'*(16-len(_bytes))+_bytes
+  for x in all_bytes:
+    # when plaintext = b'A'*16 and x = lp(b'A'): x ^ plaintext ^ lp('\x01') == 
+    if oracle(xor(lp(x),lp(b'\x01')),block): return x
+  return 'Failure'
+
 
 print(break_block(encrypt_one_token()[1][:16],check_padding))
+print(steal_last_byte(encrypt_one_token()[1][:16],check_padding))
