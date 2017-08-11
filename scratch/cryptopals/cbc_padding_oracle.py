a4eb74560e2e54686056e6beb1eedc317e22b722
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-11 18:50:22 -0400

    made things hold still for convenience

--- scratch/cryptopals/cbc_padding_oracle.py
+++ scratch/cryptopals/cbc_padding_oracle.py
@@ -3,12 +3,15 @@ from cbc import CBC_encrypt, CBC_decrypt
 from padPKCS7 import pad, unpad, PaddingError
 import random
 
-key = int.to_bytes(random.getrandbits(16*8),16,'big') # I really need to put this in a library somewhere
+# key = int.to_bytes(random.getrandbits(16*8),16,'big') # I really need to put this in a library somewhere
+key = data(b"asdfasdfasdfasdf").bytes
 def encrypt_one_token():
   tokens = ['MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
   'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=','MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==','MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==','MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl','MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==','MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==','MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=','MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=','MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93']
-  token = data(random.sample(tokens,1)[0]).bytes
-  iv = int.to_bytes(random.getrandbits(16*8),16,'big')
+  # token = data(random.sample(tokens,1)[0]).bytes
+  token = data(tokens[0]).bytes
+  # iv = int.to_bytes(random.getrandbits(16*8),16,'big')
+  iv = key
   return (iv,CBC_encrypt(pad(token,16),key,iv))
 
 def check_padding(iv,token):
@@ -27,8 +30,8 @@ def break_block(block,oracle):
     return b'\x00'*(16-len(_bytes))+_bytes
   all_bytes = [int.to_bytes(x,1,'big') for x in range(256)]
   while l() < 16:
-    padmask1 = pad(b'\x00'*(15-l()))[:16] # both padmasks have correct padding
-    padmask2 = pad(b'\xFF'*(15-l()))[:16] # therefore, (padmask ^ plaintext ^ (leftpad(right_bytes))) has correct padding
+    padmask1 = pad(b'\x00'*(15-l())) # both padmasks have correct padding
+    padmask2 = pad(b'\xFF'*(15-l())) # therefore, (padmask ^ plaintext ^ (leftpad(right_bytes))) has correct padding
     for x in all_bytes:
       test_bytes = leftpad(x+right_bytes)
       if oracle(xor(test_bytes,padmask1),block) and oracle(xor(test_bytes,padmask2),block):
