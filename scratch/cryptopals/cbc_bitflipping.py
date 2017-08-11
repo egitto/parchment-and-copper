fb4131bde7d4506f9d15be0dab5eb22fcd2d6e79
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-11 00:05:18 -0400

    removed fake solution, added real solution

--- scratch/cryptopals/cbc_bitflipping.py
+++ scratch/cryptopals/cbc_bitflipping.py
@@ -4,18 +4,12 @@ import random
 from cpt5 import data
 
 key = int.to_bytes(random.getrandbits(16*8),16,'big')
-def cyphered_comment(comment_text):
-  comment = '"comment1=cooking%20MCs;userdata="'+comment_text.replace("=","\=").replace(";","\;")+'";comment2=%20like%20a%20pound%20of%20bacon"'
+def cyphered_comment(comment_string):
+  comment = '"comment1=cooking%20MCs;userdata="'+comment_string.replace("=","\=").replace(";","\;")+'";comment2=%20like%20a%20pound%20of%20bacon"'
   return CBC_encrypt(pad(data(comment).bytes),key)
 
 def is_admin(comment):
-  print(data(unpad(CBC_decrypt(comment,key))).ascii())
+  # print(data(unpad(CBC_decrypt(comment,key))).bytes)
   return ";admin=true;" in data(unpad(CBC_decrypt(comment,key))).ascii()
 
-class fake_string(str):  #this is not the solution they had in mind :P
-  def __init__(self, contents):
-    str.__init__(self)
-  def replace(self,a,b):
-    return self
-
-print(is_admin(cyphered_comment(fake_string(";admin=true;"))))
+print(is_admin((data(cyphered_comment("\x00"*32))^data("\x00"*32+";admin=true;")).bytes))
