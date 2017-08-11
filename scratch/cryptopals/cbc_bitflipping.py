e1770f641132b117737f7d1f5af840816d5547bf
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-11 16:33:12 -0400

    probably-pointless refactoring

--- scratch/cryptopals/cbc_bitflipping.py
+++ scratch/cryptopals/cbc_bitflipping.py
@@ -4,12 +4,15 @@ import random
 from cpt5 import data
 
 key = int.to_bytes(random.getrandbits(16*8),16,'big')
-def cyphered_comment(comment_string):
-  comment = '"comment1=cooking%20MCs;userdata="'+comment_string.replace("=","\=").replace(";","\;")+'";comment2=%20like%20a%20pound%20of%20bacon"'
+def cyphered_comment(comment):
+  comment = '"comment1=cooking%20MCs;userdata="'+data(comment).ascii().replace("=","\=").replace(";","\;")+'";comment2=%20like%20a%20pound%20of%20bacon"'
   return CBC_encrypt(pad(data(comment).bytes),key)
 
 def is_admin(comment):
   # print(data(unpad(CBC_decrypt(comment,key))).bytes)
   return ";admin=true;" in data(unpad(CBC_decrypt(comment,key))).ascii()
 
-print(is_admin((data(cyphered_comment("\x00"*32))^data("\x00"*32+";admin=true;")).bytes))
+def fake_admin():
+  return (data(cyphered_comment("\x00"*320))^data("\x00"*320+";admin=true;")).bytes
+
+print(is_admin(fake_admin()))
