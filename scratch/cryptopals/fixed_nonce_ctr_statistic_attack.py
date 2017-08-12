92aa8c920aa34ca6d2960ca93a9af2c22b0f45f1
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-12 09:52:05 -0400

    solved cryptopals 20: statistical breaking of fixed-nonce ctr

---
+++ scratch/cryptopals/fixed_nonce_ctr_statistic_attack.py
@@ -0,0 +1,25 @@
+from ctr import CTR_encrypt, counter_function
+from bytestring_tools import data, chunk, xor
+from vigenere_xor import transpose_bytes, possible_bytes
+from h2b import plaintext_similarity_chi_sq as Xsq
+
+
+texts = open("cryptopals_20.txt").readlines()
+texts = [data(x.strip(),'b64').bytes for x in texts]
+cyphertexts = [CTR_encrypt(x,b"Yellow submarine",counter_function,0) for x in texts]
+
+def break_ragged_xor(cyphertexts):
+  b = transpose_bytes(sorted(cyphertexts,key=len)[::-1])
+  key = [b'\x00']*len(b)
+  for i in range(len(key)):
+    def score(test_key):
+      return (Xsq(xor(b[i],test_key*len(b[i]))),test_key)
+    key[i] = sorted(possible_bytes(),key=score)[0]
+    print(i,xor(b[i],key[i]*len(b[i])))
+  return b''.join(key)
+
+key = break_ragged_xor(cyphertexts,)
+for x in cyphertexts:
+  print(xor(x,key[:len(x)]))
+
+# print(break_vigenere(cyphertext))
