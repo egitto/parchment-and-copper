ba173f09012bff9b7c576cc80fc318c06186c18f
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-16 02:22:58 -0400

    sort of works maybe, I can't test any more for now though

--- scratch/arundel_manuscripts/download_and_stitch.js
+++ scratch/arundel_manuscripts/download_and_stitch.js
@@ -2,22 +2,16 @@ var http = require('http');
 var fs = require('fs');
 var exec = require('child_process').exec, child
 var _ = require('underscore')
+var rp = require('request-promise')
 
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close();  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    console.log(err,url,dest)
-  }).on('finish', ()=>{
-    console.log('Downloaded:',url);
-    return Promise.resolve(cb(dest))});
-  return Promise.resolve(request)
-};
+var download = (url,dest,cb)=>{
+  console.log('attempting download')
+  return rp(url).then((err,response)=>{
+    console.log('url got')
+    request(url).pipe(fs.createWriteStream(dest)).on('close', cb)
+    return Promise.resolve(cb(dest))
+  }).catch(e=>console.log(e))
+}
 
 var pad_int = (n,pad) => {
     return '0'.repeat((pad?pad:3)-(''+n).length)+n
@@ -25,36 +19,32 @@ var pad_int = (n,pad) => {
 
 var page_string = (i) =>{
   var n = pad_int(((i/2)|0)+1)
-  console.log(n.length)
   return "arundel_ms_263_f"+n+(i%2?"v":"r")
 }
 
 var get_pages = (pages,rows,cols) => {
-  if (pages.length === 0){return}
+  if (pages.length === 0){return Promise.resolve(true)}
   get_page(page_string(pages[0]),rows,cols).then((p_arr)=>{
-    console.log(p_arr)
     r = _.max(p_arr.map(x=>x[1])) //these are supposed to be objects?? not arrays??
     c = _.max(p_arr.map(x=>x[2]))
-    process_full_page(p_arr[0][0],r,c)
-    get_pages(pages.slice(1),rows,cols)
+    stitch_image(p_arr[0][0],r,c)
+    return Promise.resolve(get_pages(pages.slice(1),rows,cols))
   }).catch(e=>console.log('get_pages error:'+e))
 }
 
 var get_page = (page_string,rows,cols) => {
   if (!fs.existsSync(page_string)){
-    fs.mkdirSync(page_string);
+    fs.mkdirSync(page_string)
   }
+  console.log('get_page')
   var q = []
   _.range(rows).forEach(r=>_.range(cols).forEach(c=>q.push([page_string,r,c])))
-  get_image(...q.pop()).then(()=>{   // the last item should load completely first
-    q.map(x=>get_image(...x))        // q should now be an array of promises
-  }).catch(e=>console.log('failure in get_page',e))
+  q = q.map(x=>get_image(...x))        // q should now be an array of promises
   return Promise.all(q).catch(e=>console.log('failure in Promise.all',e))
 }
 
 var get_image = (page_string,row,col) => {
   var url = 'http://www.bl.uk/manuscripts/Proxy.ashx?view='+page_string+'_files/13/'+col+'_'+row+'.jpg'
-  console.log(url)
   var path = page_string+'/'+page_string.match(/f\d\d\d\w/)[0]+'_r'+pad_int(row)+'_c'+pad_int(col)+'.jpg'
   return download(url,path,process_downloaded)
 }
@@ -68,7 +58,7 @@ var process_downloaded = (dest) => {
   return record
 }
 
-var process_full_page = (page,rmax,cmax) => {
+var stitch_image = (page,rmax,cmax) => {
   exec('montage -geometry +0+0 -tile '+(cmax+1)+'x'+(rmax+1)+' '+page+'/* '+page+'.jpg',
     function (error, stdout, stderr) {
       console.log('stdout: ' + stdout)
@@ -78,13 +68,15 @@ var process_full_page = (page,rmax,cmax) => {
   )
 }
 
-get_pages([3,4,5],3,3)
+// to get the entire image, you'll want get_pages([indicies,12,16])
+// this is the second-highest resolution; for highest resolution, change '_files/13/' to 14
+get_pages([1],2,2)
 
 // console.log([2].map(page_string).forEach(page_string=>{
 //   // get_page(page_string,12,16)
 //   get_page(page_string,2,2)
 // }))
 
-// process_full_page('arundel_ms_263_f002v',2,2)
+// stitch_image('arundel_ms_263_f002v',2,2)
 
-// process_full_page('arundel_ms_263_f001r',12,16)
+// stitch_image('arundel_ms_263_f001r',12,16)
