99a1d74662e49396f90dba6237dcd58269414873
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-17 10:40:57 -0400

    fixed! it works quite well now

--- scratch/arundel_manuscripts/download_and_stitch.js
+++ scratch/arundel_manuscripts/download_and_stitch.js
@@ -4,6 +4,16 @@ var exec = require('child_process').exec, child
 var _ = require('underscore')
 var Promise = require('bluebird')
 var request = require('request')
+Promise.config({
+    // Enable warnings
+    warnings: true,
+    // Enable long stack traces
+    longStackTraces: true,
+    // Enable cancellation
+    cancellation: true,
+    // Enable monitoring
+    monitoring: true
+});
 
 Object.defineProperty(Array.prototype,'_',{get(){return _(this)}, configurable:true})
 
@@ -16,30 +26,36 @@ var page_string = (i) => {
   return "arundel_ms_263_f"+n+(i%2?"v":"r")
 }
 
+var get_url = (page_string,row,col) => {
+  return 'http://www.bl.uk/manuscripts/Proxy.ashx?view='+page_string+'_files/13/'+col+'_'+row+'.jpg'
+}
+
 var get_pages = (pages, rows, cols) => {
   return Promise.map(pages, (x) => get_page(page_string(x),rows,cols),{concurrency: 1})
 }
 
-var get_page = async (page_string, rows, cols) => {
-  try{fs.mkdirSync(page_string)}catch(e){} // each page gets its own folder
+var get_page = (page_string, rows, cols) => {
+  try{fs.mkdirSync('results/'+page_string)}catch(e){} // each page gets its own folder
   q = _.range(rows).map(r => _.range(cols).map(c => [page_string, r, c]))._.flatten(true) // queue
   q = Promise.map(q, x => get_image(...x),{concurrency: 1}).all()  // q is a promise of an array now
-  console.log('q promised to be mapped')
-  console.log(q)
   q = q.then((p_arr) => {
     console.log('in q.then')
+    console.log(p_arr)
     r = _.max(p_arr.map(x => x.r))
     c = _.max(p_arr.map(x => x.c))
     stitch_image(p_arr[0].id, r, c)
     return true
-  })
+  }).delay(5000)
   return q
 }
 
 var get_image = (page_string, row, col) => {
-  var url = 'http://www.bl.uk/manuscripts/Proxy.ashx?view='+page_string+'_files/13/'+col+'_'+row+'.jpg'
+  var url = get_url(page_string,row,col)
   var path = 'results/'+page_string+'/'+page_string.match(/f\d\d\d\w/)[0]+'_r'+pad_int(row)+'_c'+pad_int(col)+'.jpg'
-  return download(url, path)
+  y = download(url,path)
+    .then(() => process_downloaded(path))
+    .then((x) => {console.log('download processed',x); return x})
+  return y
 }
 
 var download = (url, dest) => {
@@ -47,15 +63,15 @@ var download = (url, dest) => {
   console.log(url)
   var y = new Promise((res,err) => {
     request(url).pipe(fs.createWriteStream(dest))
-      .on('close', () => res)
+      .on('close', res)
       .on('error', (e) => {fs.unlink(dest); err(e)})
     })
-  return y.then(process_downloaded(dest))
+  return y
 }
 
 var process_downloaded = (dest) => {
   var p = dest.match(/arundel_ms_263_f\d\d\d\w/)[0]
-  console.log(dest)
+  console.log(dest,'processing downloaded image')
   var r = parseInt(dest.match(/r\d+/)[0].slice(1))
   var c = parseInt(dest.match(/c\d+/)[0].slice(1))
   var record = {id: p, r: r, c: c}
@@ -76,7 +92,7 @@ var stitch_image = (page, rmax, cmax) => {
 
 // to get the entire image, you'll want get_pages([indicies, 12, 16])
 // this is the second-highest resolution; for highest resolution, change '_files/13/' to 14
-get_pages([1,2], 3, 3)
+get_pages(_.range(30), 12, 16)
 
 // console.log([2].map(page_string).forEach(page_string => {
 //   // get_page(page_string, 12, 16)
@@ -86,5 +102,3 @@ get_pages([1,2], 3, 3)
 // stitch_image('arundel_ms_263_f002v', 2, 2)
 
 // stitch_image('arundel_ms_263_f001r', 12, 16)
-
-a = Promise.map([1, 2, 3, 4, 5], v => new Promise.delay(100).then(() => {console.log(v);return v}), {concurrency: 2}).all().then(x => console.log(x))
