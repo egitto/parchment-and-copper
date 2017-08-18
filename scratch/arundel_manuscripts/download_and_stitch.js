4dff96ace44faea4c95e97aee718511eaa94607d
Arthur Egitto <arthur.d.egitto@gmail.com>
2017-08-18 08:13:15 -0400

    cleaned up slightly, added cases download the correctly-sized images

--- scratch/arundel_manuscripts/download_and_stitch.js
+++ scratch/arundel_manuscripts/download_and_stitch.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var exec = require('child_process').exec, child
+var exec = require('child_process').exec
 var _ = require('underscore')
 var Promise = require('bluebird')
 var request = require('request')
@@ -8,50 +8,38 @@ Promise.config({ warnings: true, longStackTraces: true, cancellation: true, moni
 
 Object.defineProperty(Array.prototype,'_',{get(){return _(this)}, configurable:true})
 
-var pad = (n, pad) => {
-    return '0'.repeat((pad?pad:3)-(''+n).length)+n
-}
+var pad = (n, pad) => '0'.repeat((pad || 3)-(''+n).length)+n
 
-var page_string = (i) => {
-  var n = pad(((i/2)|0)+1)
-  return "arundel_ms_263_f"+n+(i%2?"v":"r")
-}
+var page_string = (i) => "arundel_ms_263_f"+pad(((i/2)|0)+1)+(i%2?"v":"r")
 
-var get_url = (i, row, col, zoom) => {
-  var ps = page_string(i)
-  return 'http://www.bl.uk/manuscripts/Proxy.ashx?view='+ps+'_files/'+zoom+'/'+col+'_'+row+'.jpg'
-}
+var get_url = (i, row, col, zoom) => 'http://www.bl.uk/manuscripts/Proxy.ashx?view='+page_string(i)+'_files/'+zoom+'/'+col+'_'+row+'.jpg'
 
 var get_pages = (pages, rows, cols, zoom) => {
-  try{fs.mkdirSync('results')}catch(e){}
-  return Promise.map(pages, (x) => get_page(x, rows, cols, zoom),{concurrency: 1})
+  try {fs.mkdirSync('results')} catch(e) {}
+  return Promise.map(pages, x => get_page(x, rows, cols, zoom),{concurrency: 1}).all()
 }
 
 var get_page = (i, rows, cols, z) => {
-  var ps = page_string(i)
-  try{fs.mkdirSync('results/'+pad(i))}catch(e){} // each page gets its own folder
+  try {fs.mkdirSync('results/'+pad(i))} catch(e) {} // each page gets its own folder
   var q = rows.map(r => cols.map(c => [i, r, c, z]))._.flatten(true) // queue
   q = Promise.map(q, x => get_image(...x),{concurrency: 3}).all()  // q is a promise of an array now
-  q = q.then((p_arr) => {
+  return q.then((p_arr) => {
     // console.log('in q.then')
     // console.log(p_arr)
     r = _.max(p_arr.map(x => x.r))
     c = _.max(p_arr.map(x => x.c))
-    stitch_image(ps, r, c, i)
+    stitch_image(page_string(i), r, c, i)
     return true
   }).delay(5000)
-  return q
 }
 
 var get_image = (i, row, col, zoom) => {
   var url = get_url(i, row, col, zoom)
-  var ps = page_string(i).match(/f\d\d\d\w/)[0]
   var path = 'results/'+pad(i)+'/'+page_string(i)+'_r'+pad(row)+'_c'+pad(col)+'.jpg'
-  y = download(url, path)
+  return download(url, path)
     .then(() => process_downloaded(path,i))
-    .then((x) => {console.log('download processed', x); return x})
+    .then(x => {console.log('download processed', x); return x})
     .delay(100)
-  return y
 }
 
 var download = (url, path) => {
@@ -60,7 +48,8 @@ var download = (url, path) => {
   var y = new Promise((res, err) => {
     request(url).pipe(fs.createWriteStream(path))
       .on('close', res)
-      .on('error', (e) => {fs.unlink(path); err(e)})
+      .on('error', e => {fs.unlink(path); err(e)})
+      .on('er', e => {fs.unlink(path); err(e)})
     })
   return y
 }
@@ -69,9 +58,8 @@ var process_downloaded = (path, i) => {
   // var p = path.match(/arundel_ms_263_f\d\d\d\w/)[0]
   var r = parseInt(path.match(/r\d+/)[0].slice(1))
   var c = parseInt(path.match(/c\d+/)[0].slice(1))
-  var record = {id: i, r: r, c: c}
   if(fs.readFileSync(path).toString('utf8').slice(0, 5) === 'http:'){fs.unlinkSync(path); return {id: i, r: 0, c: 0}} // 404 error
-  return record
+  return {id: i, r: r, c: c}
 }
 
 var stitch_image = (page, rmax, cmax, i) => {
@@ -80,24 +68,19 @@ var stitch_image = (page, rmax, cmax, i) => {
     '-tile '+(cmax+1)+'x'+(rmax+1)+ // dimensions
     ' results/'+pad(i)+'/*'+ // images to stitch
     ' results/'+pad(i)+'_'+page+'.jpg', // output file
-    function (error, stdout, stderr) {
-      stdout?console.log('stdout: ' + stdout):0
-      stderr?console.log('stderr: ' + stderr):0
-      if (error !== null) {console.log('exec error: ' + error)}
+    (error, stdout, stderr) => {
+      [error,stdout,stderr].map(x => x && console.log('output/error: '+x))
     }
   )
 }
 
-// to get the entire image, you'll want get_pages([indicies, 12, 16])
-// get_pages(pagerange, rows, cols, zoom) zoom is from 1 to 14, recommend 13 for most pages. 
-get_pages([30,31], _.range(4), _.range(4), 11)
-// get_pages(_.range(15, 100), _.range(12), _.range(18), 13)
+var tall_pages = [_.range(34,40),_.range(48,60),156,157,168,169,_.range(184,194),_.range(250,258),295,296,297,_.range(300,308),_.range(368,374),384,385,398,399,412,413,418,419,420,421,426,427,446,447,460,461,462,463,560,561]._.flatten()
 
-// console.log([2].map(page_string).forEach(page_string => {
-//   // get_page(page_string, 12, 16)
-//   get_page(page_string, 2, 2)
-// }))
+var too_big_pages = [83,158,159,182,183,238,239,240,241,242,243,248,249,290,291,292,293,294,295,296,298,299,312,313,314,315,374,375,378,379,380,381,386,387,388,389,438,439,440,441,451,456,498,499,502,512,513,514,515,516,517,519,520,521,522,523,540,541,542,543,544,545,546,547,553,554,555,562,563]
 
-// stitch_image('arundel_ms_263_f002v', 2, 2)
-
-// stitch_image('arundel_ms_263_f001r', 12, 16)
+var normal_pages = _.range(0,580)._.difference(tall_pages)._.difference(too_big_pages)
+// console.log(normal_pages)
+Promise.resolve(true)
+// .then(() => get_pages(normal_pages,   _.range(7), _.range(9), 12))
+// .then(() => get_pages(tall_pages, _.range(14), _.range(9), 12)) 
+// .then(() => get_pages(too_big_pages, _.range(7),  _.range(9), 11))
